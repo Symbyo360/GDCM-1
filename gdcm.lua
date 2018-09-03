5705f96c0c26bd0277cef12ec7561b41e056e22b
MoustaphaSaadSymbyo <mostafa.saad@symbyo.com>
2018-09-03 13:45:25 +0200

    add lua file

---
+++ gdcm.lua
@@ -0,0 +1,38 @@
+project "gdcm"
+	-- uuid "C3BA937E-8404-4A94-A2AC-7D35855A6F69"
+	language "C++"
+	kind "None"
+	targetdir (bin_path .. "/%{cfg.platform}/%{cfg.buildcfg}/")
+	location (build_path .. "/gdcm/")
+
+	local gdcm_location = path.join(build_path, "gdcm")
+	gdcm_path = path.join(bin_path, "gdcm")
+
+	if os.isdir(gdcm_location) == false then
+		os.mkdir(gdcm_location)
+	end
+
+	filter "system:windows"
+
+		cleancommands { "rmdir /Q /S \"" .. gdcm_path .. "\"" }
+
+		if os.getversion().majorversion == 10.0 then
+			systemversion(win10_sdk_version())
+		end
+
+		-- i'll assume on windows you are working with vs2017 win64
+		filter "configurations:debug"
+			buildcommands
+			{
+				"cd " .. gdcm_location,
+				cmake_path .. " " .. gdcm_lib_path .. " -G \"Visual Studio 15 Win64\" -DCMAKE_INSTALL_PREFIX=\"" .. path.join(gdcm_path, "debug") .. "\" -DGDCM_BUILD_SHARED_LIBS=ON",
+				cmake_path .. " " .. " --build \"" .. gdcm_location .. "\" --target install"
+			}
+
+		filter "configurations:release"
+			buildcommands
+			{
+				"cd " .. gdcm_location,
+				cmake_path .. " " .. gdcm_lib_path .. " -G \"Visual Studio 15 Win64\" -DCMAKE_INSTALL_PREFIX=\"" .. path.join(gdcm_path, "debug") .. "\" -DGDCM_BUILD_SHARED_LIBS=ON",
+				cmake_path .. " " .. " --build \"" .. gdcm_location .. "\" --target install --config Release"
+			}
